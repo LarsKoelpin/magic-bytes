05c87a78db150101e53778b143cc297866c4bb69
Lars Kölpin-Freese <koelpin.lars+github@gmail.com>
2022-04-26 16:27:29 +0200

    fix failing tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -18,10 +18,10 @@ describe("Tests the public API", () => {
     const buffer = fs.readFileSync(require.resolve("./testfiles/a.apng"));
     const bytes = Array.prototype.slice.call(buffer, 0);
     expect(filetypeinfo(bytes)).toHaveLength(2);
-    expect(filetypeinfo(bytes)[0].typename).toBe("apng");
-    expect(filetypeinfo(bytes)[0].mime).toBe("image/apng");
-    expect(filetypeinfo(bytes)[1].typename).toBe("png");
-    expect(filetypeinfo(bytes)[1].mime).toBe("image/png");
+    expect(filetypeinfo(bytes)[1].typename).toBe("apng");
+    expect(filetypeinfo(bytes)[1].mime).toBe("image/apng");
+    expect(filetypeinfo(bytes)[0].typename).toBe("png");
+    expect(filetypeinfo(bytes)[0].mime).toBe("image/png");
   });
 
   it("detects mp4", () => {
@@ -34,14 +34,14 @@ describe("Tests the public API", () => {
 
   it("filetypeinfo", () => {
     const bytes = [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a];
-    expect(filetypeinfo(bytes)).toHaveLength(1);
+    expect(filetypeinfo(bytes)).toHaveLength(2);
     expect(filetypeinfo(bytes)[0]).toHaveProperty("typename");
   });
 
   it("filetypename", () => {
     const bytes = [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a];
-    expect(filetypename(bytes)).toHaveLength(1);
-    expect(filetypename(bytes)).toEqual(["png"]);
+    expect(filetypename(bytes)).toHaveLength(2);
+    expect(filetypename(bytes)).toEqual(["png", "apng"]);
   });
 
   it("filetypename failure", () => {
@@ -52,8 +52,8 @@ describe("Tests the public API", () => {
 
   it("filetypemime", () => {
     const bytes = [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a];
-    expect(filetypemime(bytes)).toHaveLength(1);
-    expect(filetypemime(bytes)).toEqual(["image/png"]);
+    expect(filetypemime(bytes)).toHaveLength(2);
+    expect(filetypemime(bytes)).toEqual(["image/png", "image/apng"]);
   });
 
   it("filetypemime not found", () => {
@@ -64,8 +64,8 @@ describe("Tests the public API", () => {
 
   it("filetypeextension", () => {
     const bytes = [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a];
-    expect(filetypeextension(bytes)).toHaveLength(1);
-    expect(filetypeextension(bytes)).toEqual(["png"]);
+    expect(filetypeextension(bytes)).toHaveLength(2);
+    expect(filetypeextension(bytes)).toEqual(["png", "apng"]);
   });
 
   it("filetypextension not found", () => {
